21c4ddd981c0f76dd434c88f2f7ddc0e8571b184
Jamal Yusuf <HassanJamalYusuf@gmail.com>
2016-02-01 18:40:02 -0500

    Javascript learning, hello world. Basic usage of methods prompt,alert, console.log

---
+++ Hello World/script.js
@@ -0,0 +1,3 @@
+var name = prompt("What is your name?");
+console.log("Hello " + name + "!");
+alert("I am an annoying alert, Hello World!");
---
+++ JS Stalker/script.js
@@ -0,0 +1,10 @@
+        // <li>Ask for the User's First Name</li>
+        // <li>Ask for the User's Last Name</li>
+        // <li>Ask for the User's Age</li>
+        // <li>Print out the user's full name in a sentence</li>
+        // <li>Print out the user's age in a sentence</li>
+
+
+var firstName = prompt("What is your first name?"), lastName= prompt("What is your last name?"), age= prompt("What is your age?");
+
+console.log("Hello " + firstName + " " + lastName + " you are " + age + " years old");
