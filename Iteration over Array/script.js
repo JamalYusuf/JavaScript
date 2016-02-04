33d023584492876f2552b1d8adaa7dc2b9a9cbad
Jamal Yusuf <HassanJamalYusuf@gmail.com>
2016-02-04 08:13:08 -0500

    Iteration over array elements, for loop and foreach loop

---
+++ Iteration over Array/script.js
@@ -0,0 +1,12 @@
+var colors = ["red", "blue", "green", "orange", "white", "purple"]; //general array 
+
+//if we wanted to print each item of the array
+
+for(var i = 0; i < colors.length; i++)
+	console.log(colors[i]); //prints the i'th index of colors
+
+//a foreach loop
+
+colors.forEach(function(colors){
+console.log(colors);
+});
