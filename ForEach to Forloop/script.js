7a9b4e4f8f86460954afcf09c7467cd4a0950ea3
Jamal Yusuf <HassanJamalYusuf@gmail.com>
2016-02-10 23:17:29 -0500

    Converted forEach loop into for loop

---
+++ ForEach to Forloop/script.js
@@ -0,0 +1,11 @@
+//Print out only numbers divisble by 3
+
+
+var numbers = [1,2,3,4,5,6,7,8,9];
+
+for(var i = 0; i < numbers.length; i++){
+	if(numbers[i] % 3 === 0){
+		
+		console.log(numbers[i]);
+	}
+}
