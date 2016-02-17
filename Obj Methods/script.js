48d551964f2be9279798874f3e9cb3709ae361db
Jamal Yusuf <HassanJamalYusuf@gmail.com>
2016-02-16 20:36:23 -0500

    Methods inside objects

---
+++ Obj Methods/script.js
@@ -0,0 +1,17 @@
+/*
+
+It's possible to add methods within objects 
+
+This reminds me of namespaces from java/c++
+
+*/
+
+var maths = {
+
+	add: function (x,y){
+		return x + y;
+	},
+	subtrack: function(x,y){
+		return x - y;
+	}
+}
