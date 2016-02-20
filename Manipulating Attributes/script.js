9ff44309f305b4d4070f0b302b50c0f08961606b
Jamal Yusuf <HassanJamalYusuf@gmail.com>
2016-02-19 22:11:22 -0500

    Dom setAttribute practice, Dog vs Cats page change

---
+++ Manipulating Attributes/script.js
@@ -0,0 +1,23 @@
+/*
+
+
+tag.getAttribute("href") // returns contents of href attribute
+tag.setAttribute("href", "value") //changes attribute
+
+Script to remove evil dog properganda from website
+
+*/
+
+var imageChange = document.querySelector("img");
+
+
+function catCyberWarfare()
+{
+
+document.querySelector("h1").innerHTML = "Cats are the best!";
+document.querySelector("p").innerHTML = "Cats are the smartest!";
+
+var imageChange = document.querySelector("img");
+
+imageChange.setAttribute("src", "https://www.petfinder.com/wp-content/uploads/2013/09/cat-black-superstitious-fcs-cat-myths-162286659.jpg");
+}
