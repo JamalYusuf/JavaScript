b40fe1fd2c708988b327d957d4049595fc43fdf4
Jamal Yusuf <contact@jamalyusuf.com>
2016-03-02 17:24:01 -0500

    Color picker game rework part 1

---
+++ Color Game Project 2/script.js
@@ -0,0 +1,38 @@
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var clickedColor;
+
+colorDisplay.textContent = pickedColor;
+
+
+for(var i = 0; i < squares.length; i++){
+	//add initial colors to squares
+	squares[i].style.background = colors[i];
+
+	//add click listener to squares
+	squares[i].addEventListener("click", function(){
+
+	//grab color of linked square
+	 	clickedColor = this.style.background;
+	 	console.log(clickedColor);
+
+	//compare color to pickedColor 
+	if(clickedColor === pickedColor){
+		alert("Correct!");
+	}
+	else{
+		alert("Wrong!");
+	}
+
+	});
+}
