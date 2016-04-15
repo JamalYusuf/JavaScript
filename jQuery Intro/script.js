e9a31801a9da0c28b5c584319d2f5103f245db0e
Jamal Yusuf <HassanJamalYusuf@gmail.com>
2016-04-14 20:54:46 -0400

    Introduction to jQuery

---
+++ jQuery Intro/script.js
@@ -0,0 +1,16 @@
+
+
+//Object containing css note that it must use Camal case 
+
+var style = {
+	backgroundColor: "red",
+	color: "white",
+	textDecoration: "underline",
+	border: "1px solid blue"
+}
+
+
+//Jquery Selector
+
+$("li").css(style); //Will apply style to all li elements
+
