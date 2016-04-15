730e4097bcd8cdba50f55345ee92eb4d76a6c70f
Jamal Yusuf <HassanJamalYusuf@gmail.com>
2016-04-14 21:23:40 -0400

    Excercise for jQuery

--- jQuery Intro/script.js
+++ jQuery Intro/script.js
@@ -14,3 +14,17 @@ var style = {
 
 $("li").css(style); //Will apply style to all li elements
 
+
+
+
+ //  Assignment 
+	// 1. Correctly include jQuery
+	// 2. Select all divs and give them a purple background
+	// 3. Select the divs with class "highlight" and make them 200px wide
+	// 4. Select the div with id "third" and give it a orange border 
+	// 5. Bonus: select the first div only and change its font color to pink
+
+$("div").css("background", "purple");
+$("div.highlight").css("width", "200px");
+$("div#third").css("border", "2px solid orange");
+$("div:first-of-type").css("color","pink");
