361959c75ca821c99a0c39b9858d8a955f8e4fee
Jamal Yusuf <HassanJamalYusuf@gmail.com>
2016-04-14 20:54:14 -0400

    Color game part 2, never finished it need to comeback to it. Basically I need to clean code of color game part 1 up.

--- Color Game Project 2/script.js
+++ Color Game Project 2/script.js
@@ -1,17 +1,31 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
-var clickedColor;
+var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
+resetButton.addEventListener("click", function(){
+	//generate all new colors
+	colors = generateRandomColors(6);
+
+
+	//pick a new random color from our array 
+	pickedColor =pickColor();
+	//Change color display to match picked Color 
+	colorDisplay.textContent = pickedColor; 
+
+	//change the colors of the squares on the page 
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = colors[i];
+	}
+
+	h1.style.background = "#232323";
+
+
+});
 colorDisplay.textContent = pickedColor;
 
 
@@ -23,16 +37,68 @@ for(var i = 0; i < squares.length; i++){
 	squares[i].addEventListener("click", function(){
 
 	//grab color of linked square
-	 	clickedColor = this.style.background;
+	 var clickedColor = this.style.background;
 	 	console.log(clickedColor);
 
 	//compare color to pickedColor 
+	console.log(clickedColor, pickedColor);
 	if(clickedColor === pickedColor){
-		alert("Correct!");
+		resetButton.textContent = "Play Again?";
+	 messageDisplay.textContent = "Correct!";
+	 changeColors(clickedColor);
+	 h1.style.background = clickedColor;
 	}
 	else{
-		alert("Wrong!");
+		this.style.background = "#232323";
+		messageDisplay.textContent = "Try Again!";
 	}
 
 	});
+}
+function changeColors(color){
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++){
+	//change each color to match given color 
+	squares[i].style.background = color;
+		
+	}
+
+
+
+}
+
+function pickColor(){
+	var random = Math.floor((Math.random() * colors.length ));
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make an array
+	var arr = [];
+	//add num random color to array 
+	for(var i = 0; i < num; i++){
+		//get random color and push into arr
+		arr.push(randomColor());
+
+	}
+	
+	//return array
+	return arr;
+}
+
+function randomColor(){
+
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	
+	//pick a "blue"  from 0 - 255 
+	var b = Math.floor(Math.random() * 256);
+
+
+	return "rgb("+ r + ", " + g + ", " + b + ")";
+
+
 }
